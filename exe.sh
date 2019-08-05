cce7ddb7466c89664ff859b900c12c3f89c4ba4d
trgkpc <trgkpc@gmail.com>
2019-08-05 22:36:59 +0900

    initial commit of this branch

---
+++ exe.sh
@@ -0,0 +1,2 @@
+g++ main.cpp
+./a.out > result.log
--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
 #include <iostream>
-using std::cout;
-using std::endl;
+constexpr double pi = M_PI;
+using namespace std;
 
-int main()
+#define int long long
+
+signed main()
 {
-    cout << "Hello world!" << endl;
+    double x = pi / 2;
+    int A, B;
+    cerr << "n=Aからn=Bまでを調べます。A Bを入力してください。" << endl;
+    for (int i = 0; i <= B; i++) {
+        if (i >= A) {
+            cout << i << " " << x << endl;
+        }
+        x = sin(x);
+    }
+    cerr << "終了" << endl;
 }
